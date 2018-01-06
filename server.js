72036cd33bd4a2b33fcae5b54d1259edacc543e2
zhongyu2013 <yuz871@eng.ucsd.edu>
2018-01-06 15:21:34 -0800

    Setup start script and Heroku deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -76,6 +78,6 @@ app.get
   );
 });
 
-app.listen(3000,
-() => {console.log('Server is listening on port 3000');}
+app.listen(port,
+() => {console.log(`Server is listening on port ${port}`);}
 );
